09ce5a519dd553a11ea9f0c250aca44e38e371f7
이윤빈 <kanado20000@naver.com>
2025-04-30 16:42:18 +0900

    feat: user Status enum 클래스 구현

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/user/entity/enums/Status.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/user/entity/enums/Status.java
@@ -1,4 +1,10 @@
 package com.hertz.hertz_be.domain.user.entity.enums;
 
 public enum Status {
+    GENERAL_USER("일반 사용자"),
+    MEMBERSHIP_USER("멤버십 사용자");
+
+    private final String label;
+
+    Status(String label) {this.label = label;}
 }
