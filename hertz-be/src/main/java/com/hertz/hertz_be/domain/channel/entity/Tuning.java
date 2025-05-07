7d386f9e695fab096ef451d3b9b609fe97d12097
이윤빈 <kanado20000@naver.com>
2025-05-07 16:55:09 +0900

    feat: AI서버로부터 받은 추천 리스트를 저장하기 위한 entity 클래스 추가

---
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/entity/Tuning.java
@@ -0,0 +1,41 @@
+package com.hertz.hertz_be.domain.channel.entity;
+
+import com.hertz.hertz_be.domain.channel.entity.enums.Category;
+import com.hertz.hertz_be.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Tuning {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 15)
+    private Category category;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @OneToOne(mappedBy = "tuning")
+    private TuningResult tuningResult;
+}
