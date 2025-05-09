dc1135c306e21513b63364f73de1ba8375c3c2f7
이윤빈 <kanado20000@naver.com>
2025-05-10 01:35:00 +0900

    feat: API 명세서의 공통 예외처리 모두 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/exception/GlobalExceptionHandler.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/exception/GlobalExceptionHandler.java
@@ -22,25 +22,10 @@ public class GlobalExceptionHandler {
                 .body(new ResponseDto(ex.getCode(), ex.getMessage(), null));
     }
 
-    // DTO @Valid 검증 실패
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDto<Void>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ResponseDto<>(ResponseCode.BAD_REQUEST, "잘못된 요청입니다.", null));
-    }
 
-    // JSON 형식 오류 (e.g. null, 오타 등)
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ResponseDto<Void>> handleMessageNotReadable(HttpMessageNotReadableException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ResponseDto<>(ResponseCode.BAD_REQUEST, "잘못된 요청입니다.", null));
-    }
-
-    // 경로 변수나 쿼리 파라미터 @Valid 실패 시
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ResponseDto<Void>> handleConstraintViolation(ConstraintViolationException ex) {
+    // 경로 변수나 쿼리 파라미터 @Valid 검증, DTO @Valid 검증, JSON 형식 오류 (e.g. null, 오타 등)
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    public ResponseEntity<ResponseDto<Void>> handleMessageNotReadable(Exception ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ResponseDto<>(ResponseCode.BAD_REQUEST, "잘못된 요청입니다.", null));
@@ -54,6 +39,15 @@ public class GlobalExceptionHandler {
                 .body(new ResponseDto<>(ResponseCode.NOT_FOUND, "존재하지 않는 API입니다.", null));
     }
 
+    // 비즈니스 로직에서 잡지못한 모든 예외 검증
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDto<Void>> handleException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseDto<>(ResponseCode.INTERNAL_SERVER_ERROR, "내부 서버에서 오류가 발생했습니다.", null));
+    }
+
+    // 비즈니스 로직에서의 서버 문제 검증
     @ExceptionHandler(InternalServerErrorException.class)
     public ResponseEntity<ResponseDto<Void>> handleInternalServerError(InternalServerErrorException ex) {
         return ResponseEntity
@@ -65,6 +59,7 @@ public class GlobalExceptionHandler {
                 ));
     }
 
+    // Ai 서버 문제 검증
     @ExceptionHandler(AiServerErrorException.class)
     public ResponseEntity<ResponseDto<Void>> handleAiServerError(AiServerErrorException ex) {
         return ResponseEntity
@@ -72,6 +67,18 @@ public class GlobalExceptionHandler {
                 .body(new ResponseDto<>(
                         ex.getCode(),
                         ex.getMessage(),
+                        null
+                ));
+    }
+
+    // HTTP 메서드(예: PATCH, PUT, DELETE)와 URI에 대해 컨트롤러가 존재 검증
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ResponseDto<Void>> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_IMPLEMENTED)
+                .body(new ResponseDto<>(
+                        ResponseCode.NOT_IMPLEMENTED,
+                        "요청한 URI의 메소드에 대해 서버가 구현하고 있지 않습니다.",
                         null
                 ));
     }
