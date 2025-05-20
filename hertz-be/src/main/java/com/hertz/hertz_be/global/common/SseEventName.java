20265b2b18f45081ef6b6dd4db2b5e95e655ab00
이윤빈 <kanado20000@naver.com>
2025-05-20 22:26:46 +0900

    feat: SSE 전용 ResponseCode Enum 클래스 생성

---
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/common/SseEventName.java
@@ -0,0 +1,15 @@
+package com.hertz.hertz_be.global.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SseEventName {
+
+    SIGNAL_MATCHING_CONVERSION("signal-matching-conversion"),
+    PING("ping");
+
+    private final String value;
+}
+
