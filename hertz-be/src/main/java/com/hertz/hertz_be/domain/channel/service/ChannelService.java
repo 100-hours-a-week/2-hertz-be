b2b9d252bd48495cbbf82f53e96b13e49443ea46
이윤빈 <kanado20000@naver.com>
2025-05-27 11:21:31 +0900

    refactor: Spring 5.3 이후 deprecated 될TransactionSynchronizationAdapter 대신 TransactionSynchronization 사용

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/service/ChannelService.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/service/ChannelService.java
@@ -35,6 +35,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -127,7 +128,7 @@ public class ChannelService {
 
         entityManager.flush();
 
-        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
             @Override
             public void afterCommit() {
                 asyncChannelService.sendNewMessageNotifyToPartner(signalMessage, receiver.getId(), true);
@@ -445,7 +446,7 @@ public class ChannelService {
 
         entityManager.flush();
 
-        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
             @Override
             public void afterCommit() {
                 asyncChannelService.notifyMatchingConverted(room);
