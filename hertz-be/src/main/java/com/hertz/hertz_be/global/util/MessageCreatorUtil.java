d501a772d4532a24d7c5525c499b58e3577931f2
이윤빈 <kanado20000@naver.com>
2025-06-20 11:06:57 +0900

    refactor: 메세지 생성 유틸 메서드에서 생성되는 메세지 내용 수정

--- hertz-be/src/main/java/com/hertz/hertz_be/global/util/MessageCreatorUtil.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/util/MessageCreatorUtil.java
@@ -2,11 +2,11 @@ package com.hertz.hertz_be.global.util;
 
 public class MessageCreatorUtil {
     public static String createMatchingSuccessMessage(String nickname) {
-        return String.format("🎉 축하드려요, ‘%s’님과 매칭에 성공했습니다!", nickname);
+        return String.format("🎉 축하드려요, ‘%s’님과 매칭에 성공했어요!", nickname);
     }
 
     public static String createMatchingFailureMessage(String nickname) {
-        return String.format("😥 아쉽지만, ‘%s’님과의 매칭은 성사되지 않았습니다.", nickname);
+        return String.format("😥 아쉽지만, ‘%s’님과의 매칭은 성사되지 않았어요.", nickname);
     }
 
     private MessageCreatorUtil() {}
