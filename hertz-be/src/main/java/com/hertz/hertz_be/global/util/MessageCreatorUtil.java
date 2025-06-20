a843a9a5d3eb96a348ffe9c143072512105bd46d
이윤빈 <kanado20000@naver.com>
2025-06-20 11:10:23 +0900

    feat: 튜닝레포트 메세지 생성 유틸 메서드 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/util/MessageCreatorUtil.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/util/MessageCreatorUtil.java
@@ -9,5 +9,9 @@ public class MessageCreatorUtil {
         return String.format("😥 아쉽지만, ‘%s’님과의 매칭은 성사되지 않았어요.", nickname);
     }
 
+    public static String createTuningReportMessage() {
+        return "이번 주 튜닝 결과가 왔어요! 👈확인하러가기";
+    }
+
     private MessageCreatorUtil() {}
 }
