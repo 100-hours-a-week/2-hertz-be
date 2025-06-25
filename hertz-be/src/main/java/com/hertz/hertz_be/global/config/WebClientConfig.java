189ea48a21944169b0e8b5099714f2ad753af64c
이윤빈 <kanado20000@naver.com>
2025-06-25 16:22:59 +0900

    feat: AI 서버 API 호출 로직 WebClient 구현

--- hertz-be/src/main/java/com/hertz/hertz_be/global/config/WebClientConfig.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/config/WebClientConfig.java
@@ -1,13 +1,25 @@
 package com.hertz.hertz_be.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${ai.server.ip}")
+    private String aiServerIp;
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient tuningAiServerWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl(aiServerIp)
+                .build();
     }
 }
