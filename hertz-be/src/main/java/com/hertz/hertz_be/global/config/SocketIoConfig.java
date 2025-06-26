127398dd5d38e13c0202f4729966578cba1e6792
Chaewon Lee <zzeonii0612@gmail.com>
2025-06-26 17:33:32 +0900

    feat: refreshToken 기반 인증 처리 로직 config 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/config/SocketIoConfig.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/config/SocketIoConfig.java
@@ -2,13 +2,14 @@ package com.hertz.hertz_be.global.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.Transport;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.hertz.hertz_be.global.auth.token.JwtTokenProvider;
 import com.hertz.hertz_be.global.socketio.CustomJsonSupport;
+import com.hertz.hertz_be.global.util.SocketIoTokenUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.corundumstudio.socketio.AuthorizationResult;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,6 +21,9 @@ public class SocketIoConfig {
     @Value("${socketio.server.port}")
     private int port;
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final SocketIoTokenUtil socketIoTokenUtil;
+
     @Bean
     public SocketIOServer socketIoServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
@@ -29,18 +33,19 @@ public class SocketIoConfig {
         config.setAllowCustomRequests(true);
         config.setTransports(Transport.WEBSOCKET, Transport.POLLING);
 
-        // LocalDateTime 지원하는 ObjectMapper 생성
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        // 커스텀 JsonSupport 적용
+        // 커스텀 JSON 처리기
         config.setJsonSupport(new CustomJsonSupport());
 
-        SocketIOServer server = new SocketIOServer(config);
-        server.start();
+        config.setAuthorizationListener(handshakeData -> {
+            String cookie = handshakeData.getHttpHeaders().get("cookie");
+            String refreshToken = socketIoTokenUtil.extractCookie(cookie, "refreshToken");
 
-        return server;
+            if(refreshToken != null && jwtTokenProvider.validateToken(refreshToken)) {
+                return AuthorizationResult.SUCCESSFUL_AUTHORIZATION;
+            }
+            return AuthorizationResult.FAILED_AUTHORIZATION;
+        });
 
+        return new SocketIOServer(config);
     }
-
 }
