ed8a07b5d90230d027d1457e2ba31bbb114d29f7
Chaewon Lee <87990439+zzeon9@users.noreply.github.com>
2025-06-30 23:45:31 +0900

    Fix: socket.io 서버 종료 코드 및 log 수정 (#332)
    * fix: socket.io 읽음 처리 트랜잭션 오류 처리 및 트랜잭션 로그 설정
    
    * fix: socket.io 서버 종료 코드 수정
    
    * fix: socket.io 서버 종료 코드 및 log 수정

--- hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/SocketIoServerRunner.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/SocketIoServerRunner.java
@@ -3,9 +3,11 @@ package com.hertz.hertz_be.global.socketio;
 import com.corundumstudio.socketio.SocketIOServer;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SocketIoServerRunner {
 
     private final SocketIOServer server;
@@ -21,6 +23,13 @@ public class SocketIoServerRunner {
 
     @PreDestroy
     public void stopServer() {
-        server.stop();
+        if (server != null) {
+            server.getAllClients().forEach(client -> {
+                client.disconnect();
+                log.info("🔌 클라이언트 연결 해제: sessionId = {}", client.getSessionId());
+            });
+            server.stop();
+            log.info("🧼 Socket.IO 서버 종료 완료");
+        }
     }
 }
