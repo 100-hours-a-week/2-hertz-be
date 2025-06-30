9518be3db693dd1f37263ce5a767b08273f4f41f
이윤빈 <kanado20000@naver.com>
2025-06-30 14:44:17 +0900

    feat: kafka Producer-Consumer 구조 구축

---
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.hertz.hertz_be.global.kafka.controller;
+
+
+import com.hertz.hertz_be.global.kafka.servise.KafkaProducerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/test/kafka")
+@RequiredArgsConstructor
+@Tag(name = "Kafka 관련 API")
+public class KafkaController {
+
+    private final KafkaProducerService producerService;
+
+    @PostMapping("/ping")
+    @Operation(summary = "Kafka 헬스체크를 위한 API")
+    public String sendMessage(@RequestParam String message) {
+        producerService.sendMessage("test-topic", message);
+        return "pong";
+    }
+}
