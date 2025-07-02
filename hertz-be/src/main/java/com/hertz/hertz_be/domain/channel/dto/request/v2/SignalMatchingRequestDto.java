55ee403052efda2b67f6497b07d7a5cece9d12db
Chaewon Lee <87990439+zzeon9@users.noreply.github.com>
2025-07-02 11:23:34 +0900

    Fix: Long 타입 어노테이션 수정 (#340)

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v2/SignalMatchingRequestDto.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v2/SignalMatchingRequestDto.java
@@ -1,11 +1,11 @@
 package com.hertz.hertz_be.domain.channel.dto.request.v2;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class SignalMatchingRequestDto {
 
-    @NotBlank
+    @NotNull
     private Long channelRoomId;
 }
