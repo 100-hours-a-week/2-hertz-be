9792ce1e61c5ef7de07398758dc90a21c06c23e3
이윤빈 <kanado20000@naver.com>
2025-07-03 14:23:43 +0900

    feat: SSE 이벤트 전송을 위한 Consumer/Producer 메서드 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/servise/KafkaConsumerService.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/servise/KafkaConsumerService.java
@@ -1,19 +1,40 @@
 package com.hertz.hertz_be.global.kafka.servise;
 
 
+import com.hertz.hertz_be.global.sse.SseService;
+import com.hertz.hertz_be.global.kafka.dto.SseEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaConsumerService {
+
+    private final SseService sseService;
 
     @KafkaListener(
             topics = "healthcheck-topic",
-            groupId = "${spring.kafka.consumer.healthcheck-topic.group-id}"
+            groupId = "${spring.kafka.consumer.healthcheck-topic.group-id}",
+            containerFactory = "stringKafkaListener"
     )
     public void listen(String message) {
         log.info("Kafka 메시지 수신: {}", message);
+    }
+
+    @KafkaListener(
+            topics = "${kafka.topic.sse.name}",
+            groupId = "${kafka.sse.consumer.sse-group-id}",
+            containerFactory = "sseKafkaListener"
+    )
+    public void consumeToSse(SseEvent event, Acknowledgment ack) {
+        boolean sent = sseService.sendToClient(event.userId(), event.eventName(), event.data());
+        if (sent) {
+            ack.acknowledge();
+            log.info("Kafka → SSE: userId={}, event={}", event.userId(), event.eventName());
+        }
     }
 }
--- hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/servise/KafkaProducerService.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/servise/KafkaProducerService.java
@@ -1,16 +1,26 @@
 package com.hertz.hertz_be.global.kafka.servise;
 
+import com.hertz.hertz_be.global.kafka.dto.SseEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaProducerService {
 
+    @Value("${kafka.topic.sse.name}")
+    private String SseEventTopicName;
+
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, SseEvent> kafkaTemplateForSee;
 
     public void sendMessage(String topic, String message) {
         kafkaTemplate.send(topic, message);
+    }
+
+    public void send(SseEvent event) {
+        kafkaTemplateForSee.send(SseEventTopicName, event.userId().toString(), event);
     }
 }
