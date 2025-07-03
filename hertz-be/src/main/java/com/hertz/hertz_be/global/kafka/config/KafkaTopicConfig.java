a6629b7bc16e0537093e34620be81977cdb790f4
이윤빈 <kanado20000@naver.com>
2025-07-03 14:23:02 +0900

    feat: SSE 이벤트 전송을 위한 Topic 생성 로직 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/config/KafkaTopicConfig.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/kafka/config/KafkaTopicConfig.java
@@ -1,15 +1,45 @@
 package com.hertz.hertz_be.global.kafka.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
 
+    @Value("${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Value("${kafka.topic.see.numPartitions}")
+    private String topicName;
+
+    @Value("${kafka.topic.see.numPartitions}")
+    private int numPartitions;
+
+    @Value("${kafka.topic.sse.replicationFactor}")
+    private short replicationFactor;
+
     @Bean
-    public NewTopic testTopic() {
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic sseTopic() {
+        return new NewTopic(topicName, numPartitions, replicationFactor);
+    }
+
+    @Bean
+    public NewTopic healthCheckTopic() {
         return TopicBuilder.name("healthcheck-topic")
                 .partitions(1)
                 .replicas(3)
