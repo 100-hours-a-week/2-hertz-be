c81b296ccdc369b3e3ca03b93debadcfaa5ceec2
이윤빈 <kanado20000@naver.com>
2025-07-28 10:54:03 +0900

    feat: @AllArgsConstructor 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v1/SendSignalRequestDto.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v1/SendSignalRequestDto.java
@@ -2,8 +2,10 @@ package com.hertz.hertz_be.domain.channel.dto.request.v1;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class SendSignalRequestDto {
 
