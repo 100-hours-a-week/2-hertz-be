ff8d7b278178ae79ba3cb0604ab287f3af1f0fd8
이윤빈 <81556800+kanado5385-k@users.noreply.github.com>
2025-07-29 14:55:14 +0900

    Hotfix: request dto bug fix (#415)
    * Chore: main <-> develop 브랜치 초기화 및 동기화 (#297) (#298)
    
    Co-authored-by: 이영학 <102720771+younghak9905@users.noreply.github.com>
    
    * feat: requestDto에 @NoArgsConstructor 추가
    
    ---------
    
    Co-authored-by: 이영학 <102720771+younghak9905@users.noreply.github.com>

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v3/SendSignalRequestDto.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v3/SendSignalRequestDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class SendSignalRequestDto {
 
