2efb8c12d889e0ca2c85bec549d97c5c9c96dfa2
이윤빈 <kanado20000@naver.com>
2025-07-29 14:53:29 +0900

    feat: requestDto에 @NoArgsConstructor 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v3/SendMessageRequestDto.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/channel/dto/request/v3/SendMessageRequestDto.java
@@ -3,8 +3,10 @@ package com.hertz.hertz_be.domain.channel.dto.request.v3;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class SendMessageRequestDto {
 
